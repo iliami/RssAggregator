4d514a38691e24af647c1e0d973f6f975eedcd92
iliami <ilia.mi1908@gmail.com>
2024-11-12 21:04:23 +0400

    Configured Dockerfile, docker-compose To create new init.sql: dotnet ef migrations script --idempotent --output ../RssAggregator.Database/init.sql from the RssAggregator project directory (not from the solution directory)

---
+++ RssAggregator.Database/init.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20241111064041_InitialCreate') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20241111064041_InitialCreate', '8.0.10');
+    END IF;
+END $EF$;
+COMMIT;
+
+START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20241111111512_Add-AppUser') THEN
+    CREATE TABLE "AppUsers" (
+        "Id" uuid NOT NULL,
+        "Email" text NOT NULL,
+        "Password" text NOT NULL,
+        "Role" text NOT NULL,
+        CONSTRAINT "PK_AppUsers" PRIMARY KEY ("Id")
+    );
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20241111111512_Add-AppUser') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20241111111512_Add-AppUser', '8.0.10');
+    END IF;
+END $EF$;
+COMMIT;
+
