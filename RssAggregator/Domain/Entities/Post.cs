9639e59cf81525084d7183b2b0480523d66ebe6a
iliami <ilia.mi1908@gmail.com>
2024-11-14 18:22:40 +0400

    The post entity has been configured

--- RssAggregator/Domain/Entities/Post.cs
+++ RssAggregator/Domain/Entities/Post.cs
@@ -1,18 +1,19 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace RssAggregator.Domain.Entities;
 
 public class Post
 {
     public Guid Id { get; set; }
-    [StringLength(128)]
+    [StringLength(1024)]
     public required string Title { get; set; }
-    [StringLength(8192)]
-    public required string Content { get; set; }
+    [StringLength(32768)]
+    public required string Description { get; set; }
     public DateTime PublishDate { get; set; }
     [StringLength(256)]
     public required string Url { get; set; }
     
     public Guid FeedId { get; set; }
-    public required Feed Feed { get; set; }
+    public Feed Feed { get; set; } = null!;
 }
---
+++ RssAggregator/Persistence/Migrations/20241114135605_Configure-Posts.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RssAggregator.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigurePosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "character varying(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "character varying(32768)",
+                maxLength: 32768,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1024)",
+                oldMaxLength: 1024);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
