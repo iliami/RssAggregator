8db1243236954f83d3d12baaec85ddc9c21e9752
iliami <ilia.mi1908@gmail.com>
2024-11-21 20:00:03 +0400

    Added try ... catch and logging to the sync job

--- RssAggregator/Infrastructure/BackgroundJobs/SyncFeedsService/SyncAllFeedsJob.cs
+++ RssAggregator/Infrastructure/BackgroundJobs/SyncFeedsService/SyncAllFeedsJob.cs
@@ -18,6 +18,7 @@ public class SyncAllFeedsJob(
     {
         using var scope = serviceProvider.CreateScope();
 
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<SyncAllFeedsJob>>();
         var dbContext = scope.ServiceProvider.GetRequiredService<IAppDbContext>();
 
         _feedIdPostIds = await dbContext.Feeds
@@ -29,7 +30,14 @@ public class SyncAllFeedsJob(
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            await FetchAllFeeds(stoppingToken);
+            try
+            {
+                await FetchAllFeeds(stoppingToken);
+            }
+            catch
+            {
+                logger.LogError("Error fetching all feeds.");
+            }
 
             await Task.Delay(CycleIntervalMilliseconds, stoppingToken);
         }
@@ -50,59 +58,67 @@ public class SyncAllFeedsJob(
     private async Task FetchSingleFeed(Guid feedId, CancellationToken ct = default)
     {
         using var scope = serviceProvider.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<IAppDbContext>();
-        
-        var feed = await dbContext.Feeds.FirstAsync(f => f.Id == feedId, ct);
-        
-        var httpClient = httpClientFactory.CreateClient();
-        var serializer = new XmlSerializer(typeof(RssRoot));
-        var xmlString = await httpClient.GetStringAsync(feed.Url, ct);
-        using var stringReader = new StringReader(xmlString);
-        
-        var feedFromInternet = (RssRoot)serializer.Deserialize(stringReader)!;
-        
-        var posts = feedFromInternet.Channel.Items
-            .Where(scrapedPost => 
-                (_feedIdPostIds.TryGetValue(feed.Id, out var urls) && 
-                 urls.All(url => scrapedPost.Link != url)) ||
-                urls is null)
-            .Select(scrapedPost => new Post
-            {
-                Title = scrapedPost.Title,
-                Url = scrapedPost.Link,
-                Description = scrapedPost.Description,
-                PublishDate = DateTime.Parse(scrapedPost.PubDate).ToUniversalTime(),
-            }).ToList();
-        
-        if (feed.Name != feedFromInternet.Channel.Title)
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<SyncAllFeedsJob>>();
+        try
         {
-            feed.Name = feedFromInternet.Channel.Title;
-        }
-        
-        if (feed.Description != feedFromInternet.Channel.Description)
-        {
-            feed.Description = feedFromInternet.Channel.Description;
-        }
-        
-        if (posts.Count != 0)
-        {
-            feed.Posts.AddRange(posts);
+            var dbContext = scope.ServiceProvider.GetRequiredService<IAppDbContext>();
 
-            foreach (var post in posts)
+            var feed = await dbContext.Feeds.FirstAsync(f => f.Id == feedId, ct);
+
+            var httpClient = httpClientFactory.CreateClient();
+            var serializer = new XmlSerializer(typeof(RssRoot));
+            var xmlString = await httpClient.GetStringAsync(feed.Url, ct);
+            using var stringReader = new StringReader(xmlString);
+
+            var feedFromInternet = (RssRoot)serializer.Deserialize(stringReader)!;
+
+            var posts = feedFromInternet.Channel.Items
+                .Where(scrapedPost =>
+                    (_feedIdPostIds.TryGetValue(feed.Id, out var urls) &&
+                     urls.All(url => scrapedPost.Link != url)) ||
+                    urls is null)
+                .Select(scrapedPost => new Post
+                {
+                    Title = scrapedPost.Title,
+                    Url = scrapedPost.Link,
+                    Description = scrapedPost.Description,
+                    PublishDate = DateTime.Parse(scrapedPost.PubDate).ToUniversalTime(),
+                }).ToList();
+
+            if (feed.Name != feedFromInternet.Channel.Title)
             {
-                if (_feedIdPostIds.TryGetValue(feed.Id, out var urls))
+                feed.Name = feedFromInternet.Channel.Title;
+            }
+
+            if (feed.Description != feedFromInternet.Channel.Description)
+            {
+                feed.Description = feedFromInternet.Channel.Description;
+            }
+
+            if (posts.Count != 0)
+            {
+                feed.Posts.AddRange(posts);
+
+                foreach (var post in posts)
                 {
-                    urls.Add(post.Url);
-                }
-                else
-                {
-                    _feedIdPostIds[feed.Id] = [post.Url];
+                    if (_feedIdPostIds.TryGetValue(feed.Id, out var urls))
+                    {
+                        urls.Add(post.Url);
+                    }
+                    else
+                    {
+                        _feedIdPostIds[feed.Id] = [post.Url];
+                    }
                 }
             }
+
+            feed.LastFetchedAt = DateTime.UtcNow;
+
+            await dbContext.SaveChangesAsync(ct);
         }
-        
-        feed.LastFetchedAt = DateTime.UtcNow;
-        
-        await dbContext.SaveChangesAsync(ct);
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error fetching posts from the feed {feedId}.", feedId);
+        }
     }
 }
