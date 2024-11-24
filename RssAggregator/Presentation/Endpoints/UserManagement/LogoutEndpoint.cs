739c4d3a966f268bf08c8c056dd8c76041c6efdf
iliami <ilia.mi1908@gmail.com>
2024-11-24 13:03:17 +0400

    The endpoint for logging out has been added

---
+++ RssAggregator/Presentation/Endpoints/UserManagement/LogoutEndpoint.cs
@@ -0,0 +1,30 @@
+using FastEndpoints;
+using Microsoft.Extensions.Caching.Memory;
+using RssAggregator.Presentation.Contracts.Responses.UserManagement;
+using RssAggregator.Presentation.Extensions;
+
+namespace RssAggregator.Presentation.Endpoints.UserManagement;
+
+public class LogoutEndpoint(IMemoryCache memoryCache) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Post("auth/logout");
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        var (userId, _) = User.ToIdEmailTuple();
+        
+        var key = $"userid-{userId}";
+        var tokens = memoryCache.Get<AuthResponse>(key);
+
+        if (tokens is not null)
+        {
+            memoryCache.Remove(key);
+            memoryCache.Set(tokens.AccessToken, "true", TimeSpan.FromDays(1));
+        }
+
+        return Task.CompletedTask;
+    }
+}
