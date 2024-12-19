8f60aa58259f4ee983a91a348f8f125dec4c8d2b
iliami <ilia.mi1908@gmail.com>
2024-12-19 21:46:54 +0400

    First Tests

---
+++ RssAggregator.Application.Tests/GetPostUseCaseShould.cs
@@ -0,0 +1,67 @@
+using FluentAssertions;
+using RssAggregator.Application.UseCases.Posts.GetPost;
+using NSubstitute;
+using RssAggregator.Domain.Entities;
+
+namespace RssAggregator.Application.Tests;
+
+public class GetPostUseCaseShould
+{
+    private readonly IGetPostUseCase _sut;
+    private readonly IGetPostStorage _storage = Substitute.For<IGetPostStorage>();
+    
+    public GetPostUseCaseShould()
+    {
+        _sut = new GetPostUseCase(_storage);
+    }
+    
+    [Fact]
+    public async Task ReturnEmptyGetPostResponse_WhenPostIsNotFound()
+    {
+        var postId = Guid.Parse("26060E88-B055-416A-97ED-6CBB5AB8ACF8");
+        var request = new GetPostRequest(postId);
+        _storage.TryGetAsync(postId).Returns((false, null));
+        
+        var actual = await _sut.Handle(request, CancellationToken.None);
+        
+        actual.Should().BeEquivalentTo(GetPostResponse.Empty);
+    }
+    
+    [Fact]
+    public async Task ReturnGetPostResponse_WhenPostIsFound()
+    {
+        var postId = Guid.Parse("6AE7CCA2-3D4C-4C61-B5C7-F1955409C161");
+        var post = new Post
+        {
+            Id = postId,
+            Title = "Title",
+            Description = "Description",
+            Url = "Url",
+            PublishDate = DateTime.Now,
+            Categories = [],
+            Feed = new Feed
+            {
+                Id = Guid.Empty,
+                Name = "Name",
+                Description = "Description",
+                Url = "Url",
+                LastFetchedAt = DateTimeOffset.Now,
+                Subscribers = []
+            }
+        };
+        var expected = new GetPostResponse(
+            post.Id,
+            post.Title,
+            post.Description,
+            post.Categories.Select(c => c.Name).ToArray(),
+            post.PublishDate,
+            post.Url,
+            post.Feed.Id);
+        var request = new GetPostRequest(postId);
+        _storage.TryGetAsync(postId).Returns((true, post));
+        
+        var actual = await _sut.Handle(request, CancellationToken.None);
+        
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
