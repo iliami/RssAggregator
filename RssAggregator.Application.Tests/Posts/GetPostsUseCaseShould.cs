555eb46242ebf3c44d3ae96ab5fcd20d43e95efd
iliami <ilia.mi1908@gmail.com>
2024-12-20 18:05:53 +0400

    The tests for a GetPostsUseCase have been updated

--- RssAggregator.Application.Tests/Posts/GetPostsUseCaseShould.cs
+++ RssAggregator.Application.Tests/Posts/GetPostsUseCaseShould.cs
@@ -3,7 +3,6 @@ using NSubstitute;
 using RssAggregator.Application.Models.DTO;
 using RssAggregator.Application.Models.Params;
 using RssAggregator.Application.UseCases.Posts.GetPosts;
-using RssAggregator.Domain.Entities;
 
 namespace RssAggregator.Application.Tests.Posts;
 
@@ -19,7 +18,7 @@ public class GetPostsUseCaseShould
     }
 
     [Fact]
-    public async Task ReturnEmpty_WhenNoPosts()
+    public async Task ReturnResponseWithoutPosts_WhenNoPosts()
     {
         var request = new GetPostsRequest();
         _storage
@@ -28,70 +27,68 @@ public class GetPostsUseCaseShould
                 Arg.Any<SortingParams>(),
                 Arg.Any<PostFilterParams>(),
                 Arg.Any<CancellationToken>())
-            .Returns(new PagedResult<PostDto>(Array.Empty<PostDto>(), 0));
+            .Returns(new PagedResult<PostDto>([], 0));
         var expected = new GetPostsResponse(new PagedResult<PostDto>(Array.Empty<PostDto>(), 0));
 
         var actual = await _sut.Handle(request);
 
         actual.Should().BeEquivalentTo(expected);
-        await _storage
-            .Received(1)
+        await _storage.Received(1).GetPosts(
+            Arg.Any<PaginationParams>(),
+            Arg.Any<SortingParams>(),
+            Arg.Any<PostFilterParams>(),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task ReturnResponseWithPosts_WhenRequestIsValid()
+    {
+        var posts = GeneratePosts(20);
+        var request = new GetPostsRequest(
+            new PaginationParams { Page = 1, PageSize = posts.Length },
+            new SortingParams { SortBy = "PublishDate", SortDirection = SortDirection.Desc },
+            new PostFilterParams { Categories = [] });
+        _storage
             .GetPosts(
                 Arg.Any<PaginationParams>(),
                 Arg.Any<SortingParams>(),
                 Arg.Any<PostFilterParams>(),
-                Arg.Any<CancellationToken>());
-    }
-
-    [Fact]
-    public async Task ReturnPosts_WhenRequestIsValid()
-    {
-        var posts = Enumerable.Range(0, 20)
-            .Select(_ => new PostDto(
-                Guid.Parse("2B36C46E-B804-4C2C-A5C7-3831B5377202"),
-                "",
-                [],
-                DateTime.UtcNow,
-                "",
-                Guid.Empty))
-            .ToArray();
-        var paginationParams = new PaginationParams
-        {
-            Page = 1,
-            PageSize = posts.Length
-        };
-        var sortingParams = new SortingParams
-        {
-            SortBy = string.Empty,
-            SortDirection = SortDirection.None
-        };
-        var postFilterParams = new PostFilterParams
-        {
-            Categories = []
-        };
-        
-        var request = new GetPostsRequest(
-            paginationParams,
-            sortingParams,
-            postFilterParams);
-        _storage
-            .GetPosts(
-                Arg.Is<PaginationParams>(pp => pp.Page * pp.PageSize >= posts.Length),
-                Arg.Any<SortingParams>(),
-                Arg.Is<PostFilterParams>(pfp => pfp.Categories.Length == 0),
                 Arg.Any<CancellationToken>())
-            .Returns(new PagedResult<PostDto>(posts, posts.Length));
+            .Returns(callInfo =>
+            {
+                var pagination = callInfo.Arg<PaginationParams>();
+                var page = pagination.Page;
+                var pageSize = pagination.PageSize;
+
+                var paginatedPosts = posts
+                    .Skip(( page - 1 ) * pageSize)
+                    .Take(pageSize)
+                    .ToArray();
+
+                return new PagedResult<PostDto>(paginatedPosts, posts.Length);
+            });
         var expected = new GetPostsResponse(new PagedResult<PostDto>(posts, posts.Length));
 
         var actual = await _sut.Handle(request);
 
         actual.Should().BeEquivalentTo(expected);
-        await _storage
-            .Received(1)
-            .GetPosts(
-                Arg.Any<PaginationParams>(),
-                Arg.Any<SortingParams>(),
-                Arg.Any<PostFilterParams>(),
-                Arg.Any<CancellationToken>());
+        await _storage.Received(1).GetPosts(
+            Arg.Any<PaginationParams>(),
+            Arg.Any<SortingParams>(),
+            Arg.Any<PostFilterParams>(),
+            Arg.Any<CancellationToken>());
+    }
+
+    private static PostDto[] GeneratePosts(int count)
+    {
+        return Enumerable.Range(0, count)
+            .Select(i => new PostDto(
+                Guid.NewGuid(),
+                $"Post {i}",
+                Array.Empty<string>(),
+                DateTime.UtcNow,
+                $"Content {i}",
+                Guid.NewGuid()))
+            .ToArray();
     }
 }
