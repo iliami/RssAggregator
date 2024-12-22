210b0ce547d67ef57f19985d4cde115349a2a56b
iliami <ilia.mi1908@gmail.com>
2024-12-22 20:44:28 +0400

    Validator tests for GetPostsRequest

---
+++ RssAggregator.Application.Tests/Posts/GetPosts/GetPostsRequestValidatorShould.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using RssAggregator.Application.Models.Params;
+using RssAggregator.Application.UseCases.Posts.GetPosts;
+
+namespace RssAggregator.Application.Tests.Posts.GetPosts;
+
+public class GetPostsRequestValidatorShould
+{
+    private readonly GetPostsRequestValidator _sut = new();
+    
+    [Fact]
+    public void ReturnSuccess_WhenRequestIsValid()
+    {
+        var request = new GetPostsRequest(
+            new PaginationParams(),
+            new SortingParams(),
+            new PostFilterParams([]));
+
+        var actual = _sut.Validate(request);
+
+        actual.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(GetInvalidRequests))]
+    public void ReturnFailure_WhenRequestIsInvalid(GetPostsRequest request)
+    {
+        var actual = _sut.Validate(request);
+
+        actual.IsValid.Should().BeFalse();
+    }
+
+    public static IEnumerable<object[]> GetInvalidRequests()
+    {
+        var request = new GetPostsRequest(
+            new PaginationParams(),
+            new SortingParams(),
+            new PostFilterParams([]));
+        
+        yield return [request with { PaginationParams = null! }];
+        yield return [request with { SortingParams = null! }];
+        yield return [request with { FilterParams = null! }];
+    }
+}
