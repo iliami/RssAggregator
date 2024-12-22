7590f3f99b3d2d3e107ed99e6cd1e4adbd1be0aa
iliami <ilia.mi1908@gmail.com>
2024-12-22 20:32:12 +0400

    Added storage for AddPostsInFeedUseCase

--- RssAggregator.Application/UseCases/Posts/AddPostsInFeed/IAddPostsInFeedStorage.cs
+++ RssAggregator.Application/UseCases/Posts/AddPostsInFeed/IAddPostsInFeedStorage.cs
@@ -4,6 +4,6 @@ namespace RssAggregator.Application.UseCases.Posts.AddPostsInFeed;
 
 public interface IAddPostsInFeedStorage
 {
-    Task<bool> IsFeedExists(Guid feedId);
+    Task<bool> IsFeedExists(Guid feedId, CancellationToken ct = default);
     Task AddPosts(IReadOnlyCollection<Post> posts, CancellationToken ct = default);
 }
---
+++ RssAggregator.Persistence/Storages/AddPostsInFeedStorage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RssAggregator.Application.UseCases.Posts.AddPostsInFeed;
+using RssAggregator.Domain.Entities;
+
+namespace RssAggregator.Persistence.Storages;
+
+public class AddPostsInFeedStorage(AppDbContext dbContext) : IAddPostsInFeedStorage
+{
+    public Task<bool> IsFeedExists(Guid feedId, CancellationToken ct = default)
+        => dbContext.Feeds.AnyAsync(f => f.Id == feedId, ct);
+
+    public async Task AddPosts(IReadOnlyCollection<Post> posts, CancellationToken ct = default)
+    {
+        dbContext.AttachRange(posts);
+        await dbContext.SaveChangesAsync(ct);
+    }
+}
